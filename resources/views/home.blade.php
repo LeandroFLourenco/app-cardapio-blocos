c9e4279739e1a790905149c6e04e7c7d3d4da2a0
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-02-19 14:30:23 -0300

    feat: Primeiro commit para salvar o projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu App Cardápio</title>
+</head>
+<body>
+    <h1>Bem-vindo ao App Cardápio Blocos!</h1>
+    <p>Primeira página funcionando com Laravel.</p>
+</body>
+</html>
