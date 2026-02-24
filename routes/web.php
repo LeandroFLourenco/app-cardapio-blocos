003059b92afd5832d3f8fa7a5a59e07ed2d53606
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-02-24 16:54:09 -0300

    feat:Salvando alteracoes testes para conhecimento

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CardapioController;
 
+use App\Http\Controllers\TesteController;
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/cardapio', [CardapioController::class, 'index'])->name('cardapio.index');
 Route::post('/pedidos', [CardapioController::class, 'store'])->name('pedidos.store');
+
+Route::get('/teste', [TesteController::class, 'index']);
