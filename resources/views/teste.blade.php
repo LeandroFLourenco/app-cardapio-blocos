e7cc99cd6d5a305a50657de3f34af7737b9023d5
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-03-06 16:15:41 -0300

    feat:Laravel básico: rotas, controllers, Blade, formulário POST, envio de dados, carrinho no localStorage e início da estrutura do cardápio

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 @section('title', 'Testando Blade') <!-- muda o titulo da aba da página -->
 @section('content')
-    <h1>Cardápio</h1>
-    <ul>
-        @foreach($produtos as $produto)
-            <li>{{ $produto }}</li>
-        @endforeach
-    </ul>
+<h1 class="text-2xl font-bold">Cardápio</h1>
+<div class="grid grid-cols-3 gap-6">
+
+    @foreach($produtos as $produto)
+
+        <div class="bg-white shadow-md rounded-lg p-4">
+            <h2 class="text-xl font-semibold">{{ $produto['nome'] }}</h2>
+
+            <p class="text-orange-600 font-bold">R$ {{ $produto['preco'] }}</p>
+
+            <button class="mt-3 bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600">Adicionar</button>
+        </div>
+
+    @endforeach
+
+</div>
+
 @endsection
